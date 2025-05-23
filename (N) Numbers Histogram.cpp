cc6bd056f9baa520c928c680897e6aa1f586c73d
Alfi Shahrin <arikashahrin04.95@gmail.com>
2025-05-24 00:08:11 +0600

    (N) Numbers Histogram.cpp

---
+++ (N) Numbers Histogram.cpp
@@ -0,0 +1,26 @@
+1.// Problem name:  (N) Numbers Histogram.cpp.
+2.// Course title: Competitive Programming Sessional
+3.// Course code: CCE-2310
+4.// Course teacher name: Mirza Raquib
+5.// Date: 19/5/25
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+char a;
+cin>>a;
+long long n;
+cin>>n;
+
+for(int i=1; i<=n;i++)
+{
+    long long j;
+    cin>>j;
+for(int k=1; k<=j;k++)
+{
+    cout<<a;
+}
+cout<<endl;
+}
+}
